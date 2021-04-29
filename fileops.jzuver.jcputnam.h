2589a25ed046972ea8dd59a9382cec37d7c167a5
jzuver <joshua.zuver@uvm.edu>
2021-04-29 17:57:44 -0400

    header added

---
+++ fileops.jzuver.jcputnam.h
@@ -0,0 +1,15 @@
+
+#ifndef FILEOPS_FILEOPS_JZUVER_JCPUTNAM_H
+#define FILEOPS_FILEOPS_JZUVER_JCPUTNAM_H
+#define MAXWORDLEN 31
+
+typedef struct {
+    char word[1+MAXWORDLEN];
+    long nextpos;
+} Record;
+
+
+
+
+
+#endif //FILEOPS_FILEOPS_JZUVER_JCPUTNAM_H
